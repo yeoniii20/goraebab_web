44685ff5b8789cc55b48d5710140a637314cd9b2
suyoooi <100350818+yeoniii20@users.noreply.github.com>
2024-07-23 17:03:52 +0900

    feat: add hostCard

--- src/app/(network)/network/page.tsx
+++ src/app/(network)/network/page.tsx
@@ -1,7 +1,14 @@
 'use client';
 
+import HostCard from '@/components/card/hostCard';
+
 const NetworkPage = () => {
-  return <></>;
+  return (
+    <>
+      <HostCard ip="12.34.56.67" />
+      <HostCard ip="12.34.56.67" status={false} />
+    </>
+  );
 };
 
 export default NetworkPage;
