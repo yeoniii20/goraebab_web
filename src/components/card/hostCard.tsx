20515ed9cf0952b20eeca0eb22bd199d9031af86
suyoooi <100350818+yeoniii20@users.noreply.github.com>
2024-07-23 10:42:28 +0900

    style: update hostCard

--- src/components/card/hostCard.tsx
+++ src/components/card/hostCard.tsx
@@ -7,14 +7,12 @@ type HostCardProps = {
 
 const HostCard: FC<HostCardProps> = ({ ip }) => {
   return (
-    <div className="flex items-center justify-center p-4 border border-gray-300 rounded-lg shadow-lg w-72 h-28">
-      <div className="flex items-center space-x-4">
-        <FaHome className="text-blue-500 w-8 h-8" />
-        <div>
-          <div className="text-sm text-gray-500">HOST</div>
-          <div className="text-lg font-semibold">{`eth0 : ${ip}`}</div>
-        </div>
+    <div className="flex flex-col items-center justify-center p-4 border bg-white border-grey_3 rounded-lg shadow-lg w-72 h-28">
+      <div className="flex items-center justify-center w-full space-x-2 rounded-md border-solid border-2 border-blue_2 bg-blue_1 py-2 mb-3">
+        <FaHome className="text-blue_2 w-6 h-6" />
+        <div className="text-sm text-blue_2 font-semibold">HOST</div>
       </div>
+      <div className="text-lg font-semibold">{`eth0 : ${ip}`}</div>
     </div>
   );
 };
