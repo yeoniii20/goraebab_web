d2a47c6b21b7346b2a7758d73df55317f445757b
suyoooi <100350818+yeoniii20@users.noreply.github.com>
2024-07-23 09:38:50 +0900

    style: layout 배치

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -1,20 +1,16 @@
-"use client";
+'use client';
 
-import React, { useState } from "react";
-import LeftMenu from "./leftMenu";
+import React from 'react';
+import Header from './header';
+import Sidebar from './sideBar';
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
-  const [isSidebarOpen, setSidebarOpen] = useState(false);
-
-  const closeSidebar = () => {
-    setSidebarOpen(false);
-  };
-
   return (
     <div className="relative flex h-screen bg-basic_1">
-      <LeftMenu isSidebarOpen={isSidebarOpen} onClose={closeSidebar} />
+      <Sidebar />
       <div className="flex flex-col flex-1">
-        <div className="flex-1 overflow-y-auto bg-basic_1 mt-14">
+        <Header />
+        <div className="flex-1 overflow-y-auto bg-basic_1 mt-14 p-4  ml-[300px]">
           <main>{children}</main>
         </div>
       </div>
