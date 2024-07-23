f8c8b499002f798b0ad090d25be73c3d09ab0eb8
suyoooi <100350818+yeoniii20@users.noreply.github.com>
2024-07-23 11:01:23 +0900

    style: edit padding

--- src/components/card/hostCard.tsx
+++ src/components/card/hostCard.tsx
@@ -8,7 +8,7 @@ type HostCardProps = {
 
 const HostCard: FC<HostCardProps> = ({ hostNm, ip }) => {
   return (
-    <div className="flex flex-col items-center justify-center p-4 border bg-white border-grey_3 rounded-lg shadow-lg w-72 h-28">
+    <div className="flex flex-col items-center p-[10px] border bg-white border-grey_3 rounded-lg shadow-lg w-72 h-28">
       <div className="flex items-center justify-center w-full space-x-2 rounded-md border-solid border-2 border-blue_2 bg-blue_1 py-2 mb-3">
         <FaHome className="text-blue_2 w-6 h-6" />
         <div className="text-sm text-blue_2 font-semibold">
