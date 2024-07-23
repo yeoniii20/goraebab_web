477419aaa4d213ad0f33b88b57ff54deae074327
suyoooi <100350818+yeoniii20@users.noreply.github.com>
2024-07-23 15:54:13 +0900

    design: add pan buttons

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -1,19 +1,27 @@
 'use client';
 
-import React from 'react';
+import React, { useState } from 'react';
 import Header from './header';
 import Sidebar from './sideBar';
 import ZoomButtons from '../button/zoomButtons';
+import PanButtons from '../button/panButtons';
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
+  const [isHandMode, setIsHandMode] = useState(false);
+
   return (
     <div className="relative flex h-screen bg-basic_1">
       <Sidebar />
       <div className="flex flex-col flex-1 relative">
         <Header />
-        <div className="flex-1 overflow-y-auto bg-basic_1 mt-14 p-4 ml-[300px] relative">
-          <main className="relative">{children}</main>
-          <ZoomButtons />
+        <div className="flex-1 overflow-y-auto bg-basic_1 mt-14 p-4 ml-[300px] relative bg-grey_0">
+          <main className={`relative ${isHandMode ? 'hand-mode' : ''}`}>
+            {children}
+          </main>
+          <div className="flex flex-col">
+            <ZoomButtons />
+            <PanButtons setIsHandMode={setIsHandMode} />
+          </div>
         </div>
       </div>
     </div>
