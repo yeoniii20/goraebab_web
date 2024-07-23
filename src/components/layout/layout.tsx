246f1eeba5a2bedda4a00a2da34ef85c74e98e30
suyoooi <100350818+yeoniii20@users.noreply.github.com>
2024-07-23 11:51:07 +0900

    design: add zoom button section

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -3,15 +3,17 @@
 import React from 'react';
 import Header from './header';
 import Sidebar from './sideBar';
+import ZoomButtons from '../button/zoomButtons';
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="relative flex h-screen bg-basic_1">
       <Sidebar />
-      <div className="flex flex-col flex-1">
+      <div className="flex flex-col flex-1 relative">
         <Header />
-        <div className="flex-1 overflow-y-auto bg-basic_1 mt-14 p-4  ml-[300px]">
-          <main>{children}</main>
+        <div className="flex-1 overflow-y-auto bg-basic_1 mt-14 p-4 ml-[300px] relative">
+          <main className="relative">{children}</main>
+          <ZoomButtons />
         </div>
       </div>
     </div>
