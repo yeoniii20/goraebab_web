351a718f0d1a897ff0d57f2419d1516b1b359b60
suyoooi <100350818+yeoniii20@users.noreply.github.com>
2024-07-31 17:21:00 +0900

    feat: update mock.ts

--- data/mock.ts
+++ data/mock.ts
@@ -1,4 +1,4 @@
-export const CARD_DATA = [
+export const IMAGE_CARD_DATA = [
   {
     id: 'sha-image',
     size: '124.2MB',
@@ -26,6 +26,51 @@ export const CARD_DATA = [
   {
     id: 'suyoooi-123123container',
     size: '1.2MB',
+    tags: 'httpd/latest',
+    status: 'primary',
+  },
+];
+
+export const CONTAINER_CARD_DATA = [
+  {
+    id: 'sha-container',
+    size: '124.2MB',
+    tags: 'httpd/latest',
+    status: 'primary',
+  },
+  {
+    id: 'sha-24546wdjnas930m-gkdlxkduwldk',
+    size: '35.2MB',
+    tags: 'httpd/latest',
+    status: 'secondary',
+  },
+];
+
+export const NETWORK_CARD_DATA = [
+  {
+    id: 'sha-network',
+    size: '124.2MB',
+    tags: 'httpd/latest',
+    status: 'accent',
+  },
+  {
+    id: 'sha-24546network930m-network',
+    size: '35.2MB',
+    tags: 'httpd/latest',
+    status: 'secondary',
+  },
+];
+
+export const VOLUME_CARD_DATA = [
+  {
+    id: 'volume',
+    size: '124.2MB',
+    tags: 'httpd/latest',
+    status: 'primary',
+  },
+  {
+    id: 'sha-24546network930m-network',
+    size: '35.2MB',
     tags: 'httpd/latest',
     status: 'primary',
   },
