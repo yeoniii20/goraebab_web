c1a5dc8b757bee3194899a884a7fd3cab3be3fb1
suyoooi <100350818+yeoniii20@users.noreply.github.com>
2024-07-31 17:27:34 +0900

    feat: add image page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,11 @@
 import Image from 'next/image';
 import React from 'react';
+import ImagePage from './(image)/image/page';
 
 export default function Page() {
-  return <div></div>;
+  return (
+    <div>
+      <ImagePage />
+    </div>
+  );
 }
