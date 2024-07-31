20732a62d05bd87237fd1949f237cc64f26dabee
suyoooi <100350818+yeoniii20@users.noreply.github.com>
2024-07-31 17:20:11 +0900

    feat: add card data

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -1,18 +1,45 @@
 'use client';
 
 import React, { useState } from 'react';
-import Header from './header';
-import Sidebar from './sideBar';
-import ZoomButtons from '../button/zoomButtons';
-import PanButtons from '../button/panButtons';
+import { Header, PanButtons, Sidebar, ZoomButtons } from '@/components';
+import {
+  IMAGE_CARD_DATA,
+  CONTAINER_CARD_DATA,
+  NETWORK_CARD_DATA,
+  VOLUME_CARD_DATA,
+} from '@/data/mock';
+import { useMenuStore } from '@/store/store';
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
+  const { activeId } = useMenuStore();
   const [isHandMode, setIsHandMode] = useState(false);
+
+  /**
+   * activeId에 따른 카드 데이터 변경
+   */
+  let cardData: any[];
+  switch (activeId) {
+    case 1:
+      cardData = CONTAINER_CARD_DATA;
+      break;
+    case 2:
+      cardData = IMAGE_CARD_DATA;
+      break;
+    case 3:
+      cardData = NETWORK_CARD_DATA;
+      break;
+    case 4:
+      cardData = VOLUME_CARD_DATA;
+      break;
+    default:
+      cardData = [];
+      break;
+  }
 
   return (
     <div className="relative flex h-screen bg-basic_1">
       <Header />
-      <Sidebar />
+      <Sidebar data={cardData} />
       <div className="flex flex-col flex-1 ml-[300px] mt-[56px]">
         <div className="flex-1 overflow-y-auto bg-basic_1 p-4 bg-grey_0">
           <main className={`relative ${isHandMode ? 'hand-mode' : ''}`}>
