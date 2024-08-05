58bc4d3d3b278a44ab2ab9d67f97d5d663133514
suyoooi <100350818+yeoniii20@users.noreply.github.com>
2024-08-05 17:56:10 +0900

    update: container page

--- src/app/(container)/container/page.tsx
+++ src/app/(container)/container/page.tsx
@@ -1,12 +1,40 @@
 'use client';
 
-import HostCard from '@/components/card/hostCard';
+import React from 'react';
+import { CardContainer, ConnectBar, HostCard } from '@/components';
 
-const ContainerPage = () => {
+type Container = {
+  name: string;
+  ip: string;
+  status: 'running' | 'stopped';
+};
+
+type Network = {
+  networkIp: string;
+  containers: Container[];
+};
+
+type Host = {
+  hostNm?: string;
+  ip: string;
+  status?: boolean;
+};
+
+type CardProps = {
+  host: Host;
+  network: Network;
+};
+
+const ContainerPage = ({ host, network }: CardProps) => {
   return (
-    <>
-      <HostCard ip="12.34.56.67" />
-    </>
+    <div className="flex space-x-0">
+      <HostCard hostNm={host.hostNm} ip={host.ip} status={host.status} />
+      <ConnectBar />
+      <CardContainer
+        networkIp={network.networkIp}
+        containers={network.containers}
+      />
+    </div>
   );
 };
 
