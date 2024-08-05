7bc98ee6e67c41ed799876d6e000ec9f95c41dea
suyoooi <100350818+yeoniii20@users.noreply.github.com>
2024-08-05 17:54:26 +0900

    feat: add progressBar

--- src/components/layout/sideBar.tsx
+++ src/components/layout/sideBar.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
-import { Button, Card } from '@/components';
+import { Button, Card, ProgressBar } from '@/components';
 
 interface SidebarProps {
   data: any[];
+  progress: number;
 }
 
-const Sidebar = ({ data }: SidebarProps) => {
+const Sidebar = ({ data, progress }: SidebarProps) => {
   return (
     <div className="fixed top-0 left-0 w-[300px] h-full flex flex-col bg-white p-4 border-r-2 border-grey_2 pt-20">
       <div className="flex-grow overflow-y-auto scrollbar-hide">
@@ -20,6 +21,7 @@ const Sidebar = ({ data }: SidebarProps) => {
         ))}
       </div>
       <div className="flex-shrink-0">
+        <ProgressBar progress={progress} />
         <Button title={'추가하기'} />
       </div>
     </div>
