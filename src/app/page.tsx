371a9507c0e63b4823dbbf748b99d2b8838eac31
suyoooi <100350818+yeoniii20@users.noreply.github.com>
2024-08-05 17:56:33 +0900

    update: main page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,24 @@
-import Image from 'next/image';
 import React from 'react';
 import ImagePage from './(image)/image/page';
+import ContainerPage from './(container)/container/page';
+import { HOST_DATA, NETWORK_DATA } from '@/data/mock';
+
+type Container = {
+  name: string;
+  ip: string;
+  status: 'running' | 'stopped';
+};
+
+export type Network = {
+  networkIp: string;
+  containers: Container[];
+};
 
 export default function Page() {
   return (
     <div>
       <ImagePage />
+      <ContainerPage host={HOST_DATA} network={NETWORK_DATA} />
     </div>
   );
 }
