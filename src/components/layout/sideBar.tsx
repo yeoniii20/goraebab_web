50e8f85619fb8d3e66cf9f2f4f0ffa386f875f8b
yeoniii20 <commetoi_yeoni@naver.com>
2024-08-06 22:38:28 +0900

    update: mock data

--- src/components/layout/sideBar.tsx
+++ src/components/layout/sideBar.tsx
@@ -11,13 +11,7 @@ const Sidebar = ({ data, progress }: SidebarProps) => {
     <div className="fixed top-0 left-0 w-[300px] h-full flex flex-col bg-white p-4 border-r-2 border-grey_2 pt-20">
       <div className="flex-grow overflow-y-auto scrollbar-hide">
         {data.map((item, index) => (
-          <Card
-            key={index}
-            id={item.id}
-            size={item.size}
-            tags={item.tags}
-            status={item.status}
-          />
+          <Card key={index} data={item} />
         ))}
       </div>
       <div className="flex-shrink-0">
