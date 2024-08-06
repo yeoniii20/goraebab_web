d001b9f0765ade89bd5132cb3e2859fd5f7d1985
yeoniii20 <commetoi_yeoni@naver.com>
2024-08-06 22:32:21 +0900

    fix: add handler

--- src/components/modal/optionModal.tsx
+++ src/components/modal/optionModal.tsx
@@ -4,17 +4,21 @@ import React from 'react';
 
 interface OptionModalProps {
   topTitle?: string;
+  middleTitle?: string;
   bottomTitle?: string;
   onTopHandler: () => void;
+  onMiddleHandler: () => void;
   onBottomHandler: () => void;
 }
 
-const OptionModal: React.FC<OptionModalProps> = ({
+const OptionModal = ({
   topTitle,
+  middleTitle,
   bottomTitle,
   onTopHandler,
+  onMiddleHandler,
   onBottomHandler,
-}) => {
+}: OptionModalProps) => {
   return (
     <div className="flex flex-col z-50 items-center border border-grey_3 rounded-md w-40 absolute bg-white shadow-lg">
       <button
@@ -22,6 +26,12 @@ const OptionModal: React.FC<OptionModalProps> = ({
         onClick={onTopHandler}
       >
         {topTitle || '정보 가져오기'}
+      </button>
+      <button
+        className="w-full py-1.5 text-black border-b border-grey_3 text-xs font-semibold"
+        onClick={onMiddleHandler}
+      >
+        {middleTitle || '실행 시키기'}
       </button>
       <button
         className="w-full py-1.5 text-red-500 text-xs font-semibold"
