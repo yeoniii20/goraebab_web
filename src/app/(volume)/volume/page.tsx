670bce93e05d6bd7163ca3180559eb1027ed4af5
suyoooi <100350818+yeoniii20@users.noreply.github.com>
2024-08-08 17:00:44 +0900

    feat: add components at volume page

--- src/app/(volume)/volume/page.tsx
+++ src/app/(volume)/volume/page.tsx
@@ -1,7 +1,18 @@
 'use client';
 
+import DBManagement from '@/components/docker/dbManagement';
+import DockerManagement from '@/components/docker/dockerManagement';
+import HarborManagement from '@/components/docker/harborManagement';
+
 const VolumePage = () => {
-  return <></>;
+  return (
+    <>
+      <h1>Management Dashboard</h1>
+      <DockerManagement />
+      <DBManagement />
+      <HarborManagement />
+    </>
+  );
 };
 
 export default VolumePage;
