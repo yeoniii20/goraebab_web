ba71466840d200019ed9dbf274b8497a3bc02029
suyoooi <100350818+yeoniii20@users.noreply.github.com>
2024-08-13 14:28:55 +0900

    feat: add handMode state

--- src/app/(container)/container/page.tsx
+++ src/app/(container)/container/page.tsx
@@ -1,14 +1,47 @@
 'use client';
 
-import React from 'react';
+import React, { useState } from 'react';
 import { HOST_DATA, NETWORK_DATA } from '@/data/mock';
 import CardSection from '@/components/card/cardSection';
+import { TransformProvider } from '@/context/useTransformContext';
+import useHandModeSubscription from '@/hook/useHandModeSubscription';
+import { useHandModeStore } from '@/store/handModeStore';
 
 const ContainerPage = () => {
+  // const isHandMode = useHandModeStore.getState().isHandMode;
+
+  // 상태가 변경될 때 실행할 로직을 정의합니다.
+  useHandModeSubscription((newIsHandMode) => {
+    if (newIsHandMode) {
+      console.log('Hand mode is enabled');
+      setIsHandMode(newIsHandMode);
+      // Hand mode가 활성화되었을 때 실행할 로직
+    } else {
+      console.log('Hand mode is disabled');
+      setIsHandMode(newIsHandMode);
+      // Hand mode가 비활성화되었을 때 실행할 로직
+    }
+  });
+
+  const [isHandMode, setIsHandMode] = useState(
+    useHandModeStore.getState().isHandMode
+  );
+
+  console.log(isHandMode);
+
   return (
-    <>
-      <CardSection hostData={HOST_DATA} containerData={NETWORK_DATA} />
-    </>
+    <TransformProvider>
+      <CardSection
+        hostData={HOST_DATA}
+        containerData={NETWORK_DATA}
+        isHandMode={isHandMode}
+      />
+      <CardSection
+        hostData={HOST_DATA}
+        containerData={NETWORK_DATA}
+        isHandMode={isHandMode}
+      />
+    </TransformProvider>
   );
 };
 
