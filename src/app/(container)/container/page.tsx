f90b9606fcc9b3127c811f93789bc04d2ae80335
suyoooi <100350818+yeoniii20@users.noreply.github.com>
2024-08-13 17:52:02 +0900

    Update: page.tsx

--- src/app/(container)/container/page.tsx
+++ src/app/(container)/container/page.tsx
@@ -1,46 +1,38 @@
 'use client';
 
-import React, { useState } from 'react';
-import { HOST_DATA, NETWORK_DATA } from '@/data/mock';
+import React from 'react';
+import { NETWORK_DATA } from '@/data/mock';
 import CardSection from '@/components/card/cardSection';
 import { TransformProvider } from '@/context/useTransformContext';
 import useHandModeSubscription from '@/hook/useHandModeSubscription';
-import { useHandModeStore } from '@/store/handModeStore';
+import { useHostStore } from '@/store/hostStore';
 
-const ContainerPage = () => {
-  // const isHandMode = useHandModeStore.getState().isHandMode;
+interface ContainerPageProps {
+  handleAddHost?: (hostNm: string, ip: string) => void;
+}
+
+const ContainerPage = ({ handleAddHost }: ContainerPageProps) => {
+  const [isHandMode, setIsHandMode] = React.useState(false);
+
+  // Zustand에서 hosts 데이터를 가져옵니다.
+  const hosts = useHostStore((state) => state.hosts);
 
   // 상태가 변경될 때 실행할 로직을 정의합니다.
   useHandModeSubscription((newIsHandMode) => {
-    if (newIsHandMode) {
-      console.log('Hand mode is enabled');
-      setIsHandMode(newIsHandMode);
-      // Hand mode가 활성화되었을 때 실행할 로직
-    } else {
-      console.log('Hand mode is disabled');
-      setIsHandMode(newIsHandMode);
-      // Hand mode가 비활성화되었을 때 실행할 로직
-    }
+    setIsHandMode(newIsHandMode);
   });
-
-  const [isHandMode, setIsHandMode] = useState(
-    useHandModeStore.getState().isHandMode
-  );
-
-  console.log(isHandMode);
 
   return (
     <TransformProvider>
-      <CardSection
-        hostData={HOST_DATA}
-        containerData={NETWORK_DATA}
-        isHandMode={isHandMode}
-      />
-      <CardSection
-        hostData={HOST_DATA}
-        containerData={NETWORK_DATA}
-        isHandMode={isHandMode}
-      />
+      {hosts.length > 0 &&
+        hosts.map((host, index) => (
+          <CardSection
+            key={index}
+            hostData={[host]}
+            containerData={NETWORK_DATA}
+            isHandMode={isHandMode}
+          />
+        ))}
     </TransformProvider>
   );
 };
