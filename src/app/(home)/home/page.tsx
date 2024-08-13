ea6500ed894c6ba4497c328b7ed0ba8ede115eff
suyoooi <100350818+yeoniii20@users.noreply.github.com>
2024-08-13 14:29:33 +0900

    feat: update page.tsx

--- src/app/(home)/home/page.tsx
+++ src/app/(home)/home/page.tsx
@@ -1,11 +1,33 @@
-import React from "react";
+'use client';
+
+import { ZoomButtons, PanButtons } from '@/components';
+import PanZoom from '@/components/function/panZoom';
+import PinchZoom from '@/components/function/pinchZoom';
+import { TransformProvider } from '@/context/useTransformContext';
 
 const Home = () => {
   return (
-    <>
-      <div>test</div>
-    </>
+    // <TransformProvider>
+    <div
+      style={{
+        // width: '100vw',
+        // height: '100vh',
+        width: '100%',
+        height: '100%',
+        display: 'flex',
+        flexDirection: 'column',
+      }}
+    >
+      {/* <div style={{ flex: 1 }}> */}
+      {/* <ZoomButtons />
+        <PanButtons /> */}
+      {/* <PanZoom /> */}
+      {/* </div> */}
+      {/* <div style={{ flex: 1 }}>
+        <PinchZoom />
+      </div> */}
+    </div>
+    // </TransformProvider>
   );
 };
-
 export default Home;
