b63afc66c6b144ee04a04f0d8bd75c4936f1e59a
yeoniii20 <commetoi_yeoni@naver.com>
2024-08-15 04:24:26 +0900

    update: layout and sidebar

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -12,6 +12,7 @@ import { useMenuStore } from '@/store/store';
 import AddHostButton from '../button/addHostButton';
 import { SnackbarProvider } from 'notistack';
 import SaveButton from '../button/saveButton';
+import AddBridgeButton from '../button/addBridgeButton';
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   const { activeId } = useMenuStore();
@@ -43,7 +44,6 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
     <SnackbarProvider maxSnack={3}>
       <div className="relative flex h-screen bg-basic_1 overflow-hidden">
         <Header />
-        <Sidebar data={cardData} progress={30} />
         <div className="flex flex-col flex-1 ml-[300px] mt-[56px]">
           {/* <div className="flex-1 overflow-y-auto bg-basic_1 p-4 bg-grey_0"> */}
           <div className="flex-1 bg-basic_1 bg-grey_0">
@@ -52,6 +52,7 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
             </main>
             <div className="flex flex-col">
               {/* <ZoomButtons /> */}
+              <Sidebar data={cardData} progress={30} />
               <PanButtons />
               <AddHostButton />
               <SaveButton />
--- src/components/layout/sideBar.tsx
+++ src/components/layout/sideBar.tsx
@@ -1,6 +1,11 @@
-import React from 'react';
+'use client';
+
+import React, { useState } from 'react';
 import { Button, Card, ProgressBar } from '@/components';
 import { selectedHostStore } from '@/store/seletedHostStore';
+import AddBridgeButton from '../button/addBridgeButton';
+import NetworkCard from '../card/networkCard';
+import { useMenuStore } from '@/store/store';
 
 interface SidebarProps {
   data: any[];
@@ -9,17 +14,43 @@ interface SidebarProps {
 
 const Sidebar = ({ data, progress }: SidebarProps) => {
   const { selectedHostId } = selectedHostStore();
+  const { activeId } = useMenuStore();
+
+  const [networkData, setNetworkData] = useState<any>(null);
+  console.log('networkData', networkData, '');
+
+  const handleCreateNetwork = (newNetwork: any) => {
+    console.log(newNetwork);
+    setNetworkData(newNetwork);
+  };
 
   return (
     <div className="fixed top-0 left-0 w-[300px] h-full flex flex-col bg-white p-4 border-r-2 border-grey_2 pt-20">
       <div className="flex-grow overflow-y-auto scrollbar-hide">
-        {data.map((item, index) => (
-          <Card key={index} data={item} selectedHostId={selectedHostId} />
-        ))}
+        {activeId === 3 ? (
+          <>
+            {networkData ? (
+              <NetworkCard data={networkData} selectedHostId={selectedHostId} />
+            ) : (
+              <p>네트워크 데이터를 추가하세요</p>
+            )}
+          </>
+        ) : (
+          <>
+            {data.map((item, index) => (
+              <Card key={index} data={item} selectedHostId={selectedHostId} />
+            ))}
+          </>
+        )}
       </div>
       <div className="flex-shrink-0">
         <ProgressBar progress={progress} />
-        <Button title={'추가하기'} />
+        {/* <Button title={'추가하기'} /> */}
+        {activeId === 3 ? (
+          <AddBridgeButton onCreate={handleCreateNetwork} />
+        ) : (
+          <Button title={'추가하기'} />
+        )}
       </div>
     </div>
   );
