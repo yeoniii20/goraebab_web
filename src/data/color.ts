5582fefbf18510967bf26a861f59227ec7e0c142
suyoooi <100350818+yeoniii20@users.noreply.github.com>
2024-08-14 18:28:57 +0900

    feat: create color.ts

---
+++ src/data/color.ts
@@ -0,0 +1,32 @@
+const colorOptions = ['blue', 'red', 'yellow', 'green', 'pink', 'navy'];
+
+const colorOptions1 = [
+  { blue_1: '#d2d1f6' },
+  { red_1: '#f6d4d6' },
+  { yellow_1: '#f6e3d1' },
+  { green_1: '#d1f6e2' },
+  { purple_1: '#ffe6ff' },
+  { pink_1: '#fff4f6' },
+  { navy_1: '#c3d6e1' },
+];
+
+const colorOptions2 = [
+  { blue_2: '#4C48FF' },
+  { red_2: '#FF4853' },
+  { yellow_2: '#FFA048' },
+  { green_2: '#25BD6B' },
+  { purple_2: '#e682e6' },
+  { pink_2: '#ff94aa' },
+  { navy_2: '#4b85a4' },
+];
+
+export const colorsOption = [
+  { id: 1, label: 'blue', color: '#4C48FF', sub: false },
+  { id: 2, label: 'blue', color: '#d2d1f6', sub: true },
+  { id: 3, label: 'red', color: '#FF4853', sub: false },
+  { id: 4, label: 'red', color: '#f6d4d6', sub: true },
+  { id: 5, label: 'yellow', color: '#FFA048', sub: false },
+  { id: 6, label: 'yellow', color: '#f6e3d1', sub: true },
+  { id: 7, label: 'green', color: '#25BD6B', sub: false },
+  { id: 8, label: 'green', color: '#d1f6e2', sub: true },
+];
