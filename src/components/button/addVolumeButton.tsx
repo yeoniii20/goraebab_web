872bcfdb10dc0dbec0af6d142e451236d257ccd4
suyoooi <100350818+yeoniii20@users.noreply.github.com>
2024-08-19 17:37:47 +0900

    Create addVolumeButton.tsx

---
+++ src/components/button/addVolumeButton.tsx
@@ -0,0 +1,65 @@
+'use client';
+
+import React, { useState } from 'react';
+import { useSnackbar } from 'notistack';
+import VolumeModal from '../modal/volume/volumeModal';
+import { v4 as uuidv4 } from 'uuid';
+import { showSnackbar } from '@/utils/toastUtils';
+
+interface AddVolumeButtonProps {
+  onCreate: (volumeData: any) => void;
+}
+
+const AddVolumeButton = ({ onCreate }: AddVolumeButtonProps) => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const { enqueueSnackbar } = useSnackbar();
+
+  const handleCreateVolume = (
+    id: string,
+    name: string,
+    driver: string,
+    mountPoint: string,
+    capacity: string
+  ) => {
+    const newVolumeData = {
+      id,
+      name,
+      driver,
+      mountPoint,
+      capacity,
+      status: 'available',
+    };
+
+    // 부모 컴포넌트로 생성된 볼륨 데이터 전달
+    onCreate(newVolumeData);
+    console.log('new volume ::', newVolumeData);
+
+    showSnackbar(
+      enqueueSnackbar,
+      '볼륨이 성공적으로 생성되었습니다!',
+      'success',
+      '#4C48FF'
+    );
+
+    setIsModalOpen(false);
+  };
+
+  return (
+    <>
+      <button
+        onClick={() => setIsModalOpen(true)}
+        className="mt-4 p-2 w-full text-white rounded font-bold bg-blue_2"
+      >
+        Add Volume
+      </button>
+      {isModalOpen && (
+        <VolumeModal
+          onClose={() => setIsModalOpen(false)}
+          onCreate={handleCreateVolume}
+        />
+      )}
+    </>
+  );
+};
+
+export default AddVolumeButton;
