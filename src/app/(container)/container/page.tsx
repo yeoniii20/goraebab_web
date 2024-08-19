9883a8ae6de356c7e534ead449f8b92c91217fa7
suyoooi <100350818+yeoniii20@users.noreply.github.com>
2024-08-19 14:36:50 +0900

    comment: 주석 수정

--- src/app/(container)/container/page.tsx
+++ src/app/(container)/container/page.tsx
@@ -1,20 +1,17 @@
 'use client';
 
 import React from 'react';
-import { NETWORK_DATA } from '@/data/mock';
 import CardSection from '@/components/card/cardSection';
 import { TransformProvider } from '@/context/useTransformContext';
 import useHandModeSubscription from '@/hook/useHandModeSubscription';
 import { useHostStore } from '@/store/hostStore';
 
-// Removed ContainerPageProps as it's not necessary
 const ContainerPage = () => {
   const [isHandMode, setIsHandMode] = React.useState(false);
 
-  // Zustand에서 hosts 데이터를 가져옵니다.
+  // hosts 데이터
   const hosts = useHostStore((state) => state.hosts);
- 
-  // 상태가 변경될 때 실행할 로직을 정의합니다.
+
   useHandModeSubscription((newIsHandMode) => {
     setIsHandMode(newIsHandMode);
   });
