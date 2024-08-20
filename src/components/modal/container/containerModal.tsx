39abbcd9f8bb484e620cbd5bfa91b55cd1712cef
suyoooi <100350818+yeoniii20@users.noreply.github.com>
2024-08-20 13:47:46 +0900

    feat: choose images and volumes

--- src/components/modal/container/containerModal.tsx
+++ src/components/modal/container/containerModal.tsx
@@ -4,6 +4,8 @@ import React, { useState } from 'react';
 import { v4 as uuidv4 } from 'uuid';
 import { useSnackbar } from 'notistack';
 import { showSnackbar } from '@/utils/toastUtils';
+import { useImageStore } from '@/store/imageStore';
+import { useVolumeStore } from '@/store/volumeStore';
 
 interface ContainerModalProps {
   onClose: () => void;
@@ -13,12 +15,25 @@ interface ContainerModalProps {
 const ContainerModal = ({ onClose, onSave }: ContainerModalProps) => {
   const [name, setName] = useState('');
   const [ip, setIp] = useState('');
-  const [size, setSize] = useState('');
+  const [ports, setPorts] = useState('80:80,443:443');
+  const [selectedVolumes, setSelectedVolumes] = useState<string[]>([]); // 선택된 볼륨 저장
+  const [network, setNetwork] = useState('bridge');
   const [tags, setTags] = useState('');
+  const [selectedImage, setSelectedImage] = useState(''); // 선택된 이미지 저장
   const { enqueueSnackbar } = useSnackbar();
+  const images = useImageStore((state) => state.images); // 이미지 스토어에서 이미지 목록 가져오기
+  const volumes = useVolumeStore((state) => state.volumes); // 볼륨 스토어에서 볼륨 목록 가져오기
+
+  const handleVolumeChange = (volumeName: string) => {
+    setSelectedVolumes((prevSelected) =>
+      prevSelected.includes(volumeName)
+        ? prevSelected.filter((name) => name !== volumeName)
+        : [...prevSelected, volumeName]
+    );
+  };
 
   const handleSave = () => {
-    if (!name || !ip || !size) {
+    if (!name || !selectedImage || !ip || selectedVolumes.length === 0) {
       showSnackbar(
         enqueueSnackbar,
         '모든 필드를 입력해주세요.',
@@ -31,11 +46,13 @@ const ContainerModal = ({ onClose, onSave }: ContainerModalProps) => {
     const newContainer = {
       id: uuidv4(),
       name,
+      image: selectedImage,
       ip,
-      size,
-      tags,
-      active: 'running',
-      status: 'primary',
+      status: 'running',
+      ports: ports.split(',').map((port) => port.trim()),
+      volumes: selectedVolumes,
+      network,
+      tags: tags.split(',').map((tag) => tag.trim()),
     };
 
     onSave(newContainer);
@@ -43,8 +60,8 @@ const ContainerModal = ({ onClose, onSave }: ContainerModalProps) => {
   };
 
   return (
-    <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50">
-      <div className="bg-white p-6 rounded-md shadow-lg">
+    <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50 p-32">
+      <div className="bg-white p-6 rounded-md shadow-lg w-3/5">
         <h2 className="text-lg font-semibold mb-4">Add New Container</h2>
         <input
           type="text"
@@ -53,6 +70,42 @@ const ContainerModal = ({ onClose, onSave }: ContainerModalProps) => {
           onChange={(e) => setName(e.target.value)}
           className="mb-2 p-2 border border-gray-300 rounded w-full"
         />
+        {/* 이미지 선택 드롭다운 */}
+        <select
+          value={selectedImage}
+          onChange={(e) => setSelectedImage(e.target.value)}
+          className="mb-2 p-2 border border-gray-300 rounded w-full"
+        >
+          <option value="" hidden>
+            Select an Image
+          </option>
+          {images.map((image) => (
+            <option key={image.id} value={image.name}>
+              {image.name} ({image.source})
+            </option>
+          ))}
+        </select>
+
+        {/* 볼륨 선택 체크박스 */}
+        <p className="font-semibold mb-2">Select Volumes:</p>
+        <div className="mb-4 max-h-24 overflow-y-auto border p-2 rounded">
+          {volumes.map((volume) => (
+            <div key={volume.id} className="flex items-center mb-2">
+              <input
+                type="checkbox"
+                id={`volume-${volume.id}`}
+                value={volume.name}
+                checked={selectedVolumes.includes(volume.name)}
+                onChange={() => handleVolumeChange(volume.name)}
+                className="mr-2"
+              />
+              <label htmlFor={`volume-${volume.id}`}>
+                {volume.name} ({volume.driver})
+              </label>
+            </div>
+          ))}
+        </div>
+
         <input
           type="text"
           placeholder="IP Address"
@@ -62,9 +115,16 @@ const ContainerModal = ({ onClose, onSave }: ContainerModalProps) => {
         />
         <input
           type="text"
-          placeholder="Size (MB)"
-          value={size}
-          onChange={(e) => setSize(e.target.value)}
+          placeholder="Ports (e.g., 80:80,443:443)"
+          value={ports}
+          onChange={(e) => setPorts(e.target.value)}
+          className="mb-2 p-2 border border-gray-300 rounded w-full"
+        />
+        <input
+          type="text"
+          placeholder="Network"
+          value={network}
+          onChange={(e) => setNetwork(e.target.value)}
           className="mb-2 p-2 border border-gray-300 rounded w-full"
         />
         <input
